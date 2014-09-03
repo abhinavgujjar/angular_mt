3f14f200308dc4ed796730244fdc37adbe86ccb8
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-09-03 14:44:56 +0530

    custom directives

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,20 @@
-angular.module('hotelmgr', ['ngRoute']);
+angular.module('hotelmgr', ['ngRoute', 'ngSanitize', 'ui.bootstrap.tpls', 'ui.bootstrap.tooltip']);
 
-angular.module('hotelmgr').config(function($routeProvider){
+angular.module('hotelmgr').config(function($routeProvider) {
 	$routeProvider.when('/listing', {
-		templateUrl : 'partials/listing.html'
+		templateUrl: 'partials/listing.html'
 	});
 
 	$routeProvider.when('/create', {
-		templateUrl : 'partials/create.html'
+		templateUrl: 'partials/create.html'
 	});
 
-$routeProvider.when('/details/:hotelId', {
-		templateUrl : 'partials/details.html'
+	$routeProvider.when('/details/:hotelId', {
+		templateUrl: 'partials/details.html'
 	});
 
-	$routeProvider.otherwise( {
-		redirectTo : '/listing'
+	$routeProvider.otherwise({
+		redirectTo: '/listing'
 	});
 
 })
@@ -30,8 +30,22 @@ angular.module('hotelmgr').value('config', {
 });
 
 angular.module('hotelmgr').value('defaultHotel', {
-		name : 'config default',
-		price : 10000,
-		description : 'Some sort of description'
-	});
+	name: 'config default',
+	price: 10000,
+	description: 'Some sort of description'
+});
 
+
+angular.module('hotelmgr').directive('preview', function() {
+
+	//directive definition object
+
+	return {
+		restrict: 'EA',
+		templateUrl: 'partials/preview.html',
+		scope: {
+			input : '=text'
+		}
+	}
+
+});
